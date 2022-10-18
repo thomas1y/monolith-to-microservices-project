9181ad7187d415ffc5c7458635555f71c2dcc207
unknown <thomas1y>
2022-10-18 14:11:36 -0400

    add new frontend version

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a11efa3441ea14ed79a4d284b99060a4-1002335203.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
